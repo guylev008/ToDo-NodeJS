901be64e6a074015efe27aea75208d5f70e44cad
guy <guy.selenium@gmail.com>
2020-11-21 17:37:19 +0200

    remove sorting and pagination

--- src/routers/user.js
+++ src/routers/user.js
@@ -66,13 +66,4 @@ router.patch('/users/me', auth, async (req, res) => {
 	}
 });
 
-router.delete('/users/me', auth, async (req, res) => {
-	try {
-		await req.user.remove();
-		res.send(req.user);
-	} catch (error) {
-		res.status(500).send(error);
-	}
-});
-
 module.exports = router;
